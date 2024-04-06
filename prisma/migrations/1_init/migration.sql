f1a8e2d3769ddebb1a3693847704fe7212680310
reidwids <reidwids@gmail.com>
2024-04-05 22:07:15 -0400

    add db initial migration

---
+++ prisma/migrations/1_init/migration.sql
@@ -0,0 +1,35 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "ipfs";
+
+-- CreateTable
+CREATE TABLE "ipfs"."File" (
+    "id" TEXT NOT NULL,
+    "filename" TEXT NOT NULL,
+    "cid" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ipfs"."User" (
+    "id" TEXT NOT NULL,
+    "publicAddress" TEXT NOT NULL,
+    "nonce" TEXT NOT NULL,
+    "nonceExpiry" TIMESTAMP(3) NOT NULL,
+    "email" TEXT,
+    "username" TEXT NOT NULL,
+    "preferredNetwork" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_publicAddress_key" ON "ipfs"."User"("publicAddress");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "ipfs"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "ipfs"."File" ADD CONSTRAINT "File_userId_fkey" FOREIGN KEY ("userId") REFERENCES "ipfs"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
