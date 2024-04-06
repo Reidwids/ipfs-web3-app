d1710ea74ad87a91d055a536bd0ea4cfff61e6d2
reidwids <reidwids@gmail.com>
2024-04-05 21:32:54 -0400

    Add file and user tables to db

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,14 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
-declare global {
-  var prisma: PrismaClient | undefined;
-}
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma as any) {
+  if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
   prisma = global.prisma;
